a3739f31a4dac25d6200dd88c959d5f9702294d9
robertca <robertca@rose-hulman.edu>
2019-02-03 18:57:01 -0500

    DONE

--- src/problem0.py
+++ src/problem0.py
@@ -9,8 +9,8 @@ These problems illustrate concepts that previous problems have not emphasized:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Valerie Galluzzi, Mark Hays, Amanda Stouder, Aaron Wilkin,
-         their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues, and Drew Roberts.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 import testing_helper
@@ -189,7 +189,7 @@ def problem0a(n):
            since (2 + 4 + 6) is 12, which is NOT odd.
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -206,6 +206,13 @@ def problem0a(n):
     #        Simply try a few examples to convince yourself of this.
     #        ASK FOR HELP if you do not understand this hint.
     # -------------------------------------------------------------------------
+    answer = sum_of_digits(n)
+    y = answer % 2
+    if y == 0:
+        x = False
+    else:
+        x = True
+    return x
 
 
 def run_test_problem0b():
@@ -262,7 +269,7 @@ def problem0b(n):
            since there are 46 primes between 2 and 200.
      """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -271,6 +278,11 @@ def problem0b(n):
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     ###########################################################################
     # ------------------------------------------------------------------
+    total = 0
+    for k in range(2, n+1):
+        if is_prime(k):
+            total = total + 1
+    return total
 
 
 def run_test_problem0c():
@@ -338,6 +350,14 @@ def problem0c(circle, n, window):
     #   renders with a half-second pause after rendering.
     ###########################################################################
     # -------------------------------------------------------------------------
+
+    radius = circle.radius
+    center = circle.center
+    for k in range(n + 1):
+        circles = rg.Circle(rg.Point(center.x + 2 * k * radius, center.y), radius)
+        circles.attach_to(window)
+        circle.attach_to(window)
+        window.render(0.5)
 
 
 ###############################################################################
